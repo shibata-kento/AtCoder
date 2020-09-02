d1dda3466b1c3e379d1901a7bebdabcdb68d754a
shibata-kento <shibata0203.jp@gmail.com>
2020-09-02 23:05:18 +0900

    [add] ABC107 AB

--- ABC107/bb.py
+++
@@ -1,9 +0,0 @@
-h, w = map(int, input().split())
-a = [input() for i in range(h)]
-for i in range(h):
-    tmp = ''
-    if a[i].count('.') != w:
-        for j in range(w):
-            tmp += a[j][i]
-        if '#' in tmp:
-            print(a[i])
