45912e1b4e1975b8e1674fc7fd8695b100d13e5c
shibata-kento <shibata0203.jp@gmail.com>
2020-09-04 22:38:22 +0900

    [add] ABC110 AB

---
+++ ABC110/b.py
@@ -0,0 +1,12 @@
+n, m, X, Y = map(int, input().split())
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
+x = sorted(x, reverse=False)[-1]
+y = sorted(y, reverse=False)[0]
+flg = 'War'
+if x < x+1 <= y:
+    if x+1 > X:
+        if x+1 <= Y:
+            flg = 'No War'
+
+print(flg)    
