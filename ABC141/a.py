4f72b48db3bc111be056db53d6a9e5fa2bbf12f8
shibata-kento <shibata0203.jp@gmail.com>
2020-10-17 13:41:10 +0900

    [add] ABC141 AB

---
+++ ABC141/a.py
@@ -0,0 +1,7 @@
+s = input()
+if s == 'Sunny':
+    print('Cloudy')
+elif s == 'Rainy':
+    print('Sunny')
+elif s == 'Cloudy':
+    print('Rainy')
---
+++ ABC141/b.py
@@ -0,0 +1,13 @@
+s = input()
+
+for i in range(len(s)):
+    if i % 2 == 0:
+        if s[i] == 'L':
+            print('No')
+            exit()
+
+    else:
+        if s[i] == 'R':
+            print('No')
+            exit()
+print('Yes')
