ce3494a04409e72cc1111edd185318bf408ce87d
shibata-kento <shibata0203.jp@gmail.com>
2020-10-25 16:31:29 +0900

    [add] ABC177 AB

--- ABC177/b.py
+++ ABC177/b.py
@@ -1,15 +1,17 @@
 s = input()
 t = input()
-ans = 10**9
-string = ''
+st = len(s) * len(t)
 if t in s:
     print(0)
+    exit()
 else:
     for i in range(len(s) - len(t) + 1):
-        string = s[i:i+len(t)]
         c = 0
-        for  j, k in enumerate(string):
-            if t[j] != k:
-                c += 1
-        ans = min(ans, c)
-print(ans)
+        tmp = 0
+        for j in range(len(t)):
+            if s[i+j] != t[j]:
+                tmp += 1
+        c = st
+        if c > tmp:
+            st = tmp
+print(st)
